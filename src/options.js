3d5b29ab75a050fe8ecdc8d15dd6324e8721cf9a
Dev-AlexYT <dev.mynzz@gmail.com>
2025-04-18 16:06:35 -0500

    new file:   src/options.js

---
+++ src/options.js
@@ -0,0 +1,17 @@
+    //? Optional: Permissions that the bot requires to execute the command.
+    //? botPermissions: ['SendMessages'], // Example: bot needs permission to send messages.
+
+    //? Optional: Permissions that the user requires to use this command. Uncomment if needed.
+    //? userPermissions: ['ManageMessages'], // Example: Only users with Manage Messages permission can use this command.
+
+    //? Optional: Set this to true if only bot admins can use this command.
+    //? adminOnly: true,
+
+    //? Optional: Set this to true if only the bot owner can use this command.
+    //? ownerOnly: true,
+
+    //? Optional: Set this to true if only developers can use this command.
+    //? devOnly: true, so if this true this slash command will only register for the server IDs you provided in config.json
+
+    //? Optional: Cooldown period for the command in seconds to prevent spam.
+    //? cooldown: 10,
